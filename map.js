b9f6ce80977309c54fc86615569596c8af2aea16
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-05-22 22:35:38 -0700

    step2

--- map.js
+++ map.js
@@ -1,2 +1,33 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3VwczIwMDQiLCJhIjoiY21iMGM3aHhlMHQ5MzJycHQ5bXE0aXNyMyJ9.UD9HZjjx97TMurbVHF7Jag';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/outdoors-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
+
+map.on('load', async () => {
+  map.addSource('boston_route', {
+  type: 'geojson',
+  data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+});
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+    'line-color': '#32D400',  // A bright green using hex code
+    'line-width': 5,          // Thicker lines
+    'line-opacity': 0.6       // Slightly less transparent
+  },
+  });
+});
+
